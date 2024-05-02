7f6e28ceb296957281c823bd314f1efeeecc1ad2
NAINAYUNIZA14 <nainayuniza29@gmail.com>
2024-05-02 10:53:41 +0700

    TugasPertemuan1.php

---
+++ application/controllers/TugasPertemuan1.php
@@ -0,0 +1,12 @@
+<?php
+class TugasPertemuan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya NAINA YUNIZA 
+            saya tinggal di tanjung raya 2
+            hobby saya berolahraga bola volly dan nonton drakor";
+    }
+    
+}
